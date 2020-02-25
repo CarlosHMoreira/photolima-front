25dc7f4401a3efd280caaf2aef66e3e75ed43560
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-02-25 04:46:10 -0300

    Create a animations file

---
+++ src/ui/Animations.jsx
@@ -0,0 +1,23 @@
+import { keyframes } from 'styled-components';
+
+const shakeIt = keyframes`
+  from {
+    transform: rotate(0);
+  }
+
+  30% {
+    transform: rotate(20deg);
+  }
+
+  60% {
+    transform: rotate(-20deg);
+  }
+
+  to {
+    transform: rotate(0);
+  }
+`;
+
+export default {
+  shakeIt,
+};
