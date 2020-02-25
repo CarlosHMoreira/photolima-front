46097700e560635df2835964a6e6a30b9030a316
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-02-25 01:13:19 -0300

    Adds rules to images and fonts and resolve alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
@@ -17,10 +18,23 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      ui: path.resolve(__dirname, 'src', 'ui'),
+      images: path.resolve(__dirname, 'src', 'assets', 'images'),
+      fonts: path.resolve(__dirname, 'src', 'assets', 'fonts'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
